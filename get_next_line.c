73727fd3ce126c9234ecc337f58061394b01aba4
Anastasiia Lavrukhina <alavrukh@student.hive.fi>
2025-05-28 15:20:37 +0300

    md

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alavrukh <alavrukh@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 14:45:22 by alavrukh          #+#    #+#             */
-/*   Updated: 2025/05/28 13:52:57 by alavrukh         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:11:34 by alavrukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,19 @@
 
 char *get_next_line(int fd)
 {
-    static char buffer[BUFFER_SIZE];
-    static int  start = 0;
-    static int  end = 0;
-    int i = 0;
-    char *line = malloc(BUFFER_SIZE + 1);
+	char *line;
 
-    if (!line)
-        return NULL;
+	line = malloc (BUFFER_SIZE +1);
+	if (!line)
+		return NULL;
 
-    while (1)
-    {
-        if (start >= end) 
-        {
-            end = read(fd, buffer, BUFFER_SIZE);
-            start = 0;
-            if (end <= 0)  
-            {
-                free(line);
-                return NULL;
-            }
-        }
-
-        line[i] = buffer[start];
-        start++;
-        i++;
-
-        if (line[i - 1] == '\n')
-            break;
-
-        if (i >= BUFFER_SIZE)
-            break;
-    }
-
-    line[i] = '\0';
-
-    return line;
+	ssize_t bytes_read = read (fd, line, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(line);
+		return NULL;
+	}
+	line[bytes_read] = '\0';
+	return line;
 }
+
